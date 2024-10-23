ed98fe85b056820b461ad542c09f49180db34005
Benjamin <benjamin.braden@lions.nfl.net>
2024-10-22 22:40:05 -0400

    get content links tests

--- src/mgoblog_data/common/web_scraper.py
+++ src/mgoblog_data/common/web_scraper.py
@@ -20,3 +20,26 @@ class MGoBlogWebScraper:
         
         else:
             raise Exception(f"Error in accessing web page. Received status code {r.status_code}.")
+        
+    def get_content_links(self, url_content: str) -> list[str]:
+        """
+            Parses a list of content links from a MGoBlog web page.
+
+            Args:
+                url_content (str): Content of an MGoBlog page in a str
+            
+            Returns:
+                list[str]: list of links to content pages found on web page
+        """
+        soup = BeautifulSoup(url_content, 'html.parser')
+
+        content_links = []
+        title_divs = soup.find_all("div", class_="title-wrapper")
+        for title_div in title_divs:
+            anchor_tag = title_div.find("a")
+
+            if anchor_tag:
+                content_links.append(anchor_tag["href"])
+    
+        return content_links
+
--- src/mgoblog_data/tests/test_web_scraper.py
+++ src/mgoblog_data/tests/test_web_scraper.py
@@ -17,3 +17,29 @@ def test_invalid_url_fails():
     
     with pytest.raises(Exception):
         ws._get_web_page(url=invalid_url)
+
+def test_get_content_links():
+    ws = MGoBlogWebScraper()
+
+    sample_html = """
+                 <html>
+                    <body>
+                        <div class="title-of-title-wrapper">
+                                <a href="wrong">Anchor tag edge case</a>
+                            <div class='title-wrapper'>
+                                <a href="right"></a>
+                            </div>
+                        </div>
+                        <div class='not-title-wrapper'>
+                            <a href="wrong"/>
+                        </div>
+                        <div class='title-wrapper'>
+                            <a href="right"/>
+                        </div>
+                    </body>
+                 </html>
+    """
+    content_links = ws.get_content_links(url_content=sample_html)
+    assert len(content_links) == 2
+    assert content_links[0] == "right"
+    assert content_links[1] == "right"
