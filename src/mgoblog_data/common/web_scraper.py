a61703e098321e0c41207602e6ff866556e4fc36
Benjamin <benjamin.braden@lions.nfl.net>
2024-10-27 21:25:48 -0400

    web scraper

--- src/mgoblog_data/common/web_scraper.py
+++ src/mgoblog_data/common/web_scraper.py
@@ -7,7 +7,7 @@ class MGoBlogWebScraper:
         """
             Retrieves the web page at the given url and returns the text content of the page.
 
-            If web page cannot be reached, function will throw an exception.
+            If web page cannot be reached, function will return an empty string.
 
             Args:
                 url (str): URL of a web page.
@@ -19,22 +19,27 @@ class MGoBlogWebScraper:
             return r.text
         
         else:
-            raise Exception(f"Error in accessing web page. Received status code {r.status_code}.")
+            return ""
         
-    def get_content_links(self, url_content: str) -> list[str]:
+    def get_content_links(self, page_content: str) -> list[str]:
         """
             Parses a list of content links from a MGoBlog web page.
 
             Args:
-                url_content (str): Content of an MGoBlog page in a str
+                page_content (str): HTML of page that content links will be parsed from
             
             Returns:
                 list[str]: list of links to content pages found on web page
         """
-        soup = BeautifulSoup(url_content, 'html.parser')
+
+        soup = BeautifulSoup(page_content, 'html.parser')
 
         content_links = []
         title_divs = soup.find_all("div", class_="title-wrapper")
+
+        if title_divs is None:
+            return content_links
+
         for title_div in title_divs:
             anchor_tag = title_div.find("a")
 
@@ -42,4 +47,129 @@ class MGoBlogWebScraper:
                 content_links.append(anchor_tag["href"])
     
         return content_links
+    
+    def _get_more_content_link(self, page_content: str) -> str:
+        """
+            Gets link to more content from page. 
+            
+            There are two page patterns that we are looking for.
+
+            1) A more articles button located on the bottom of the page
+
+            2) A list with links to numbered pages with a next button at the end.
+
+            Function will look for one of the two buttons for a link to more content and return the link if it's found
+
+            Args:
+                page_contant (str): HTML of page to extract link to more content from
+            
+            Returns:
+                str: link to more content. Will return None if no such link is found
+        """
+        soup = BeautifulSoup(page_content, 'html.parser')
+        more_content_dict = soup.find("div", class_="more-link")
+        next_page_li = soup.find("li", class_="pager__item--next")
+
+        if more_content_dict is None and next_page_li is None:
+            return None
+
+        if more_content_dict is None:
+            anchor_tag = next_page_li.find("a")
+        else:
+            anchor_tag = more_content_dict.find("a")
+
+        if anchor_tag is not None:
+            return anchor_tag["href"]
+        
+        else:
+            return None
+
+    def _get_page_content(self, page_content: str) -> dict:
+        """
+            Extract the data from a content page.
+
+            Args:
+                page_content (str): HTML of page to extract content data from
+
+            Returns:
+                dict: content data to return from page
+        """
+
+        soup = BeautifulSoup(page_content, 'html.parser')
+
+        article = soup.find("article")
+
+        if article is None:
+            return {}
+
+        # Extract article tags
+        tags = []
+        try:
+            tag_div_items = article.find("div",class_="field--name-tags").find_all("div", class_="field__item")
+            for tag_div_item in tag_div_items:
+                tags.append(tag_div_item.text)
+        
+        except:
+            pass # If tag divs not found, do nothing
+
+        # Extract author
+        author_div = article.find("span", class_="field--name-uid")
+        if author_div is not None:
+            author = author_div.text
+        else:
+            author = None
+
+        # Extract page title
+        title_header = article.find("h1", class_="page-title")
+        if title_header is not None:
+            title = title_header.text
+        else:
+            title = None
+
+        # Extract page body content
+        body_div = article.find("div", class_="field--name-body")
+        if body_div is not None:
+            body = body_div.text
+        else:
+            body = None
+
+        return {"tags": tags, "title": title, "author": author, "body": body}
+    
+    def get_content(self, start_url: str, max_iteration: int=5, iteration: int=0, results: list[dict]=[]) -> list[dict]:
+        """
+            Finds links to content on Mgoblog page, gets content data from links, and recursively iterates process to next page.
+
+            Args:
+                start_url (str): Initial page to start scraping content from
+                max_iterations (int): Max number of pages to look for content on
+                iteration (int): Number of pages iterated through
+        """
+
+        if iteration == max_iteration:
+            return results
+        
+        start_page_content = self._get_web_page(url=start_url)
+
+        content_links = self.get_content_links(page_content=start_page_content)
+
+        # Iterate through content pages one by one and retrieve data
+        for content_link in content_links:
+            page_content = self._get_web_page(url=content_link)
+
+            if len(page_content) > 0:
+                retr_data = self._get_page_content(page_content=page_content)
+
+                if len(retr_data) > 0:
+                    results.append(retr_data)
+        
+        more_content_link = self._get_more_content_link(page_content=start_page_content)
+
+        if more_content_link is None:
+            return results
+        
+        else:
+            return self.get_content(start_url=more_content_link, max_iteration=max_iteration, iteration=iteration+1, results=results)
+        
+
+
 
--- src/mgoblog_data/tests/test_web_scraper.py
+++ src/mgoblog_data/tests/test_web_scraper.py
@@ -14,9 +14,8 @@ def test_invalid_url_fails():
     ws = MGoBlogWebScraper()
 
     invalid_url = "https://www.espn.com/junk"
-    
-    with pytest.raises(Exception):
-        ws._get_web_page(url=invalid_url)
+
+    assert ws._get_web_page(url=invalid_url) == ""
 
 def test_get_content_links():
     ws = MGoBlogWebScraper()
@@ -39,7 +38,97 @@ def test_get_content_links():
                     </body>
                  </html>
     """
-    content_links = ws.get_content_links(url_content=sample_html)
+    content_links = ws.get_content_links(page_content=sample_html)
     assert len(content_links) == 2
     assert content_links[0] == "right"
     assert content_links[1] == "right"
+
+def test_get_page_content():
+
+    ws = MGoBlogWebScraper()
+
+    sample_html = """
+        <html>
+            <body>
+                <article class="irrelevant class names">
+                    <div class="field--name-tags">
+                        <div class="field__item">First Tag</div>
+                        <div class="field">Second Tag</div>
+                        <div class="field__item">Third Tag</div>
+                        <div class="__item">Fourth Tag</div>
+                    </div>
+                    <span class="field--name-uid">Ben Braden</span>
+                    <h1 class="not-page-title">
+                        Wrong Test Article
+                    </h1>
+                    <h1 class="page-title other-names">Test Article</h1>
+                    <div class="field--name-body other-class-name test_div">
+                        <p>Paragraph One</p>
+                        <p>Paragraph Two</p>
+                    </div>
+                    <div class="body">
+                        <p>Paragraph Three</p>
+                    </div>
+                </article>
+            </body>
+        </html>
+    """
+    content_data = ws._get_page_content(page_content=sample_html)
+
+    assert content_data['tags'] == ['First Tag', 'Third Tag']
+    assert content_data['author'] == "Ben Braden"
+    assert content_data['title'] == "Test Article"
+    assert "Paragraph One" in content_data["body"]
+    assert "Paragraph Two" in content_data["body"]
+    assert "Paragraph Three" not in content_data["body"]
+
+def test_get_more_content_link_success():
+
+    ws = MGoBlogWebScraper()
+
+    sample_html = """
+                    <html>
+                        <body>
+                            <div class="more-link">
+                                <a href="/more_content"></a>
+                            </div>
+                        </body>
+                    </html>
+                    """
+    
+    more_content_link = ws._get_more_content_link(sample_html)
+
+    assert more_content_link == "/more_content"
+
+def test_get_more_content_link_fail():
+    ws = MGoBlogWebScraper()
+
+    sample_html = """
+                    <html>
+                        <body>
+                            <div class="more-link">
+                            </div>
+                        </body>
+                    </html>
+                    """
+    
+    more_content_link = ws._get_more_content_link(sample_html)
+
+    assert more_content_link == None
+
+def test_get_more_content_link_success_next_page():
+    ws = MGoBlogWebScraper()
+
+    sample_html = """
+                    <html>
+                        <body>
+                            <li class="pager__item--next">
+                                <a href="/more_content"></a>
+                            </div>
+                        </body>
+                    </html>
+                    """
+    
+    more_content_link = ws._get_more_content_link(sample_html)
+
+    assert more_content_link == "/more_content"
