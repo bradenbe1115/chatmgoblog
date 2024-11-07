eea5b6bc6da93c55026ffc0a934dce1a688fbc42
Benjamin <benjamin.braden@lions.nfl.net>
2024-11-06 23:10:35 -0500

    test updates

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -6,10 +6,10 @@ DB_NAME = "test_db"
 RAW_DATA_COLLECTION_NAME = "test_repository"
 
 def test_mgoblog_content_repository():
-    repo = PyMongoMgoBlogContentRepository(db_uri=DB_URI, database_name=DB_NAME, raw_mgoblog_content_collection_name=RAW_DATA_COLLECTION_NAME)
+    repo = PyMongoMgoBlogContentRepository(db_uri=DB_URI, landing_database_name=DB_NAME, mgoblog_content_collection_name=RAW_DATA_COLLECTION_NAME)
 
-    data = [MgoblogContentLandingDataSchema(url="/url_one", raw_html="Hello World", collected_ts="2024-01-01"),
-            MgoblogContentLandingDataSchema(url="/url_two", raw_html="Hello World Again", collected_ts="2024-02-01")]
+    data = [MgoblogContentLandingDataSchema(url="/url_one", raw_html="Hello World", collected_ts=1234),
+            MgoblogContentLandingDataSchema(url="/url_two", raw_html="Hello World Again", collected_ts=1234)]
     
     repo.add_raw_mgoblog_content(data=data)
 
@@ -18,7 +18,7 @@ def test_mgoblog_content_repository():
     assert retr_entry["raw_html"] == "Hello World"
     assert repo.get_raw_mgoblog_content(url="/url_two") is not None
 
-    new_data = [MgoblogContentLandingDataSchema(url="/url_one", raw_html="Hello World - Test Update", collected_ts="2024-01-01")]
+    new_data = [MgoblogContentLandingDataSchema(url="/url_one", raw_html="Hello World - Test Update", collected_ts=1234)]
 
     repo.add_raw_mgoblog_content(new_data)
     retr_entry = repo.get_raw_mgoblog_content(url="/url_one")
