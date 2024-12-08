5781c47bc74cacde18db5d306d0d397cd1376d11
Benjamin <benjamin.braden@lions.nfl.net>
2024-12-07 21:05:44 -0500

    new sentence chunker

--- src/embed/common/chunker.py
+++ src/embed/common/chunker.py
@@ -1,5 +1,6 @@
 import abc
 import nltk
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 nltk.download('punkt_tab')
 nltk.download('punkt')
@@ -11,7 +12,7 @@ class AbstractChunker(abc.ABC):
         Each instance of this class must provide a chunk_text method that implements the given chunking strategy. 
     """
 
-    def chunk_text(self, data: str) -> list[list[str]]:
+    def chunk_text(self, data: str, **kwargs) -> list[list[str]]:
         """
             Breaks provided text data into chunks using a strategy specific to the class implementation.
 
@@ -20,18 +21,35 @@ class AbstractChunker(abc.ABC):
             Returns:
                 A list of lists of strings, where each inner list contains the chunks generated from the corresponding text in the original list.
         """
-        return self._chunk_text(data)
+        return self._chunk_text(data,**kwargs)
     
     @abc.abstractmethod
-    def _chunk_text(self, data: str) -> list[list[str]]:
+    def _chunk_text(self, data: str, **kwargs) -> list[list[str]]:
         raise NotImplementedError
     
 class SentenceChunker(AbstractChunker):
 
-    def _chunk_text(self, data):
+    def _chunk_text(self, data, **kwargs):
         return nltk.sent_tokenize(data)
 
 class IdentityChunker(AbstractChunker):
 
-    def _chunk_text(self, data):
+    def _chunk_text(self, data, **kwargs):
         return data
+    
+class RecursiveCharacterTextChunker(AbstractChunker):
+
+    def _chunk_text(self, data, **kwargs):
+
+        chunk_size = kwargs.get("chunk_size", 500)
+        chunk_overlap = kwargs.get("chunk_overlap",50)
+
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len,
+            is_separator_regex=False,
+        )
+        
+        return text_splitter.split_text(data)
--- tests/unit/test_sentence_chunker.py
+++ tests/unit/test_sentence_chunker.py
@@ -1,4 +1,4 @@
-from embed.common.chunker import SentenceChunker
+from embed.common.chunker import SentenceChunker, RecursiveCharacterTextChunker
 
 def test_basic_paragraph():
     test_data = "Hello there. I'm a paragraph of text designed to test a chunker. Isn't working with LLMs fun!"
@@ -15,3 +15,16 @@ def test_paragraph_with_weird_patterns():
     chunker = SentenceChunker()
     results = chunker.chunk_text(test_data)
     assert len(results) == 2
+
+def test_recursive_character_text_chunker():
+    with open("/tests/unit/long_text.txt", "r") as file:
+        content = file.read()
+
+    chunker = RecursiveCharacterTextChunker()
+    results = chunker.chunk_text(content)
+
+    assert len(results) > 0
+
+    results_two = chunker.chunk_text(content, chunk_size=20, chunk_overlap=10)
+
+    assert len(results_two) > len(results)
