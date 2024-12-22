b965090fae659444e1a446f57a5cba2ac2a5f21a
Benjamin <benjamin.braden@lions.nfl.net>
2024-12-21 23:26:01 -0500

    tests update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
+import time
 import pymongo
 import chromadb
+from pathlib import Path
+import requests
+from user_query import config as user_query_config
+from tenacity import retry, stop_after_delay
+
+pytest.register_assert_rewrite("tests.e2e.api_client")
 
 @pytest.fixture
 def test_elt_db_client():
@@ -14,3 +21,13 @@ def test_chromadb_client():
     host, port = "http://test_vector_db:6333", 6333
     client = chromadb.HttpClient(host, port)
     yield client
+
+@retry(stop=stop_after_delay(10))
+def wait_for_webapp_to_come_up():
+    return requests.get(user_query_config.get_api_url())
+
+@pytest.fixture
+def restart_api():
+    (Path(__file__).parent / "../../pipelines/user_query.py").touch()
+    time.sleep(0.5)
+    wait_for_webapp_to_come_up()
